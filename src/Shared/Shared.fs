2b44d679d6f8b12ac65fc28a146d6d7d0f05801c
markpattison <mr.pattison@gmail.com>
2021-07-10 15:00:30 +0100

    add server files and data

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -19,3 +19,41 @@ module Route =
 type ITodosApi =
     { getTodos: unit -> Async<Todo list>
       addTodo: Todo -> Async<Todo> }
+
+type Loop =
+    {
+        LatLongs: (float * float) []
+    }
+
+type Shape =
+    {
+        OuterBoundary: Loop
+        Holes: Loop []
+    }
+
+type Boundary =
+    {
+        Shapes: Shape []
+    }
+
+type CovidRates =
+    {
+        WeeklyCasesPer100k: Map<DateTime, float>
+    }
+
+type ONSCode = | ONSCode of string
+
+type Area =
+    {
+        ONSCode: ONSCode
+        Name: string
+        Boundary: Boundary
+        Data: CovidRates option
+    }
+
+// fsharplint:disable RecordFieldNames
+
+type ICovidMapApi =
+    { getDates : unit -> Async<DateTime []>
+      getData : unit -> Async<Area []>
+    }
